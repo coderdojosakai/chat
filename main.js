16a2591ae0b8a74db3bee259f229a3c5138a21eb
coderdojo-sakai <coderdojo.sakai@gmail.com>
2016-03-05 17:35:31 +0900

    user add

--- main.js
+++ main.js
@@ -4,29 +4,31 @@ var milkcocoa = new MilkCocoa('postilb7c65u.mlkcca.com');
 var chatDataStore = milkcocoa.dataStore("chat");
 var textArea, board;
 window.onload = function(){
+  userName = document.getElementById("usr");
   textArea = document.getElementById("msg");
   board = document.getElementById("board");
 }
  
 function clickEvent(){
   var text = textArea.value;
-  sendText(text);
+  var user = userName.value;
+  sendText(user,text);
 }
  
-function sendText(text){
-  chatDataStore.push({message : text},function(data){
+function sendText(user,text){
+  chatDataStore.push({name: user, message : text},function(data){
     console.log("送信完了!");
     textArea.value = "";
   });
 }
  
 chatDataStore.on("push",function(data){
-  addText(data.value.message);
+  addText(data.value.name, data.value.message);
 });
  
-function addText(text){
+function addText(user,text){
   var msgDom = document.createElement("li");
-  msgDom.innerHTML = text;
+  msgDom.innerHTML = user + ":" + text;
   board.insertBefore(msgDom, board.firstChild);
 }
 
